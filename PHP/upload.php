1d2fccc0b3a9596d26a1389f3070b38df5f22bc9
William-Arthur Cinq-Mars <will_arth53@hotmail.com>
2021-03-03 14:03:24 -0500

    upload de photode profile finit

--- PHP/upload.php
+++ PHP/upload.php
@@ -1,6 +1,8 @@
 <?php
+$matricule = $_POST["matricule"];
+
 $target_dir = "../RESSOURCE/Picture/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . $matricule . ".png";
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
@@ -8,41 +10,36 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo json_encode("File is an image - " . $check["mime"] . ".");
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo json_encode("File is not an image.");
     $uploadOk = 0;
   }
 }
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
+  echo json_encode("Sorry, your file is too large.");
   $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "png") {
-  echo "Only PNG files are allowed.";
+  echo json_encode("Only PNG files are allowed.");
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo json_encode("Sorry, your file was not uploaded.");
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo json_encode("le fichier ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " a bien ete uploader.");
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo json_encode("Desoler un erreure c'est produit.");
   }
 }
 ?>
