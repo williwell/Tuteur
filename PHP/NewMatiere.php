7834d6abfc39d58996faaf88fdc03854c3cec316
William-Arthur Cinq-Mars <will_arth53@hotmail.com>
2021-03-04 08:46:59 -0500

    Push final des ajout de matiere avec verif

--- PHP/NewMatiere.php
+++ PHP/NewMatiere.php
@@ -11,21 +11,28 @@
         $description = $_POST['description'];
 
         $lines = $query->checkMatiere($fichier,$noCour);
-        if($lines < 1)
+        if(sizeof($lines) < 1)
         {
-            $target_dir = "../Cours/$noCour/";
-            if(!is_dir($target_dir)){
-                mkdir($target_dir);
+            $lines = $query->getInfoCour($noCour);
+            if(sizeof($lines) == 1)
+           {
+               $target_dir = "../Cours/$noCour/";
+                if(!is_dir($target_dir)){
+                    mkdir($target_dir);
+                }
+    
+                $target_file = $target_dir . $fichier;
+    
+                if (move_uploaded_file($_FILES["fichier"]["tmp_name"], $target_file)) {
+                    $lines = $query->newMatiere($fichier,$noCour,$title,$description);
+                    echo json_encode("Ajout reussit");
+                } else {
+                    echo json_encode("Desoler, un erreure a empeche l'ajaout du fichier.");
+                }
             }
-    
-            $target_file = $target_dir . $fichier;
-    
-            if (move_uploaded_file($_FILES["fichier"]["tmp_name"], $target_file)) {
-                //echo json_encode("Le fichier ". htmlspecialchars($fichier). " a bien ete ajouter sur le serveur.");
-                $lines = $query->newMatiere($fichier,$noCour,$title,$description);
-                echo json_encode("Ajout reussit");
-            } else {
-                echo json_encode("Desoler, un erreure a empeche l'ajaout du fichier.");
+            else
+            {
+                echo json_encode("Le numero de cours n'est pas vailde!");
             }
         }
         else{
